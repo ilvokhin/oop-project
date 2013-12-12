69f758de14abc2050efebc4c0e2b4884232067f2
Steve Hideandseekey <eveningsteps@gmail.com>
2013-12-12 12:11:08 +0400

    let text field to enable/disable 'Send' button

--- src/ChatTab.py
+++ src/ChatTab.py
@@ -16,11 +16,19 @@ class ChatTab (QtGui.QWidget):
         QtGui.QWidget.__init__(self, parent)
         self.ui = uic.loadUi (("./ui/chatTab.ui"), self)
         self.closeButton.clicked.connect(self.closeButton_clicked)
+        self.messageField.textChanged.connect (self.text_changed)
         self.id = id
 
     def closeButton_clicked (self):
         self.close()
         self.closeButton.raise_()
+
+    def text_changed (self):
+        st = self.messageField.toPlainText()
+        if st.size() > 0:
+            self.sendButton.setEnabled (True)
+        else:
+            self.sendButton.setEnabled (False)
 
 def main():
         app = QtGui.QApplication(sys.argv)
