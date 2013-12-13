5e45e2595a5eae296d65342d9e3c55fe34957cb5
Dima Ilvokhin <r3tsky@gmail.com>
2013-12-13 20:37:00 +0400

    add popup support

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -12,6 +12,7 @@ from Registry import Registry
 #from VkClient import VkClient
 from Config import Config
 from ChatWindow import ChatWindow
+from PopUp import PopUpMan
 
 class MainWindow(QtGui.QMainWindow):
 	def __init__(self, parent = None):
@@ -22,10 +23,12 @@ class MainWindow(QtGui.QMainWindow):
 		self.registry = Registry()
 		self.registry.objects['config'] = Config()
 		self.new_messages = {}
+		self.popup_man = PopUpMan()
 		
 		# connect widgets and slots
 		self.loginButton.clicked.connect(self.loginButton_clicked)
                 self.contactList.itemDoubleClicked.connect (self.contactListEntry_doubleclicked)
+		self.connect(self.popup_man, self.popup_man.signal, self.openChatTabFromPopUp)
 
                 # TODO: add a button for configuration window
 		if self.registry.objects['config'].isLogin():
@@ -80,7 +83,10 @@ class MainWindow(QtGui.QMainWindow):
 		vk = self.registry.objects['vk']
 		msgs.pop(0)
 		mark_as_read = []
-		self.new_messages.clear()
+		old_messages = self.new_messages
+		cnt = 0
+		#self.new_messages.clear()
+		self.new_messages = {}
 		for msg in reversed(msgs):
 			if hasattr(self, 'ChatWindow') and msg['uid'] in self.ChatWindow.tabs:
 				uid = msg['uid']
@@ -93,6 +99,12 @@ class MainWindow(QtGui.QMainWindow):
 				#Change icon in contact list
   				print "Chat doesn't open"
 				print msg
+				if (msg['uid'] not in old_messages or
+					(msg['uid'] in old_messages 
+					and msg['mid'] not in old_messages[msg['uid']])
+				   ):
+					self.popup_man.create(msg['uid'], msg['mid'], msg['body'], cnt)
+					cnt += 1
 				if msg['uid'] in self.new_messages:
  					self.new_messages[msg['uid']].append(msg['mid'])
 				else:
@@ -101,21 +113,39 @@ class MainWindow(QtGui.QMainWindow):
 			vk.markAsRead(mark_as_read)
 		if self.new_messages or mark_as_read:
 			self.UpdateContactList(vk.online)
-
+	
+	def openChatTab(self, id, show_history_mes = 3):
+		if not hasattr(self, 'ChatWindow'):
+			self.ChatWindow = ChatWindow()
+		vk = self.registry.objects['vk']
+		name = vk.id_to_name[id]
+		self.ChatWindow.addChatTab(id, name, show_history_mes)
+		self.ChatWindow.show()
+	
+	def openChatTabFromPopUp(self, id):
+		vk = self.registry.objects['vk']
+		vk.markAsRead(self.new_messages[id])
+                self.openChatTab(id, len(self.new_messages[id]))
+                del self.new_messages[id]
+		self.UpdateContactList(vk.online)
+		print "open chattab from popup", id
+	
         def contactListEntry_doubleclicked (self, entry):
                 name = entry.text()
 		vk = self.registry.objects['vk']
-		if not hasattr(self, 'ChatWindow'):
-			self.ChatWindow = ChatWindow()
+		#if not hasattr(self, 'ChatWindow'):
+		#	self.ChatWindow = ChatWindow()
                 id = vk.name_to_id[unicode (name)]
 		if id in self.new_messages:
-			self.ChatWindow.addChatTab (id, name, len(self.new_messages[id]))
+			#self.ChatWindow.addChatTab (id, name, len(self.new_messages[id]))
 			vk.markAsRead(self.new_messages[id])
+			self.openChatTab(id, len(self.new_messages[id]))
 			del self.new_messages[id]
 			self.UpdateContactList(vk.online)
 		else:
-                	self.ChatWindow.addChatTab (id, name)
-                self.ChatWindow.show()
+                 	#self.ChatWindow.addChatTab (id, name)
+			self.openChatTab(id)
+                #self.ChatWindow.show()
  
 
 def main():
